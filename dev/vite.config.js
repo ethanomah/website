e53d8c6b879e7c587fee0abbd485c3531b613a8a
Ethan O'Mahony <ethan@tramore.org>
2024-08-16 23:08:26 +0100

    updates for deployment

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // build: {
-  //   outDir: "../docs"
-  // }
+  build: {
+    outDir: "../docs"
+  }
 })
