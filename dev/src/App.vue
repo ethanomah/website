28fc66da9815622a4de6d6545b986a7e5838a0f4
Ethan O'Mahony <ethan@tramore.org>
2024-08-19 13:11:16 +0100

    Added sidebar component

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -1,20 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
+import SidebarComponent from './components/SidebarComponent.vue'
 </script>
 
 <template>
   <canvas style="position: absolute; left:0; top:0; z-index:-1; width: 100%; height: 100%; opacity: 0.4;">
   </canvas>
-  <div :class="['sidebar', { open: isOpen }]">
-    <button @click="toggleSidebar" class="toggle-btn">☰</button>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-  </div>
+  <SidebarComponent />
 
   <header>
     <div class="container-fluid" style="align: center; text-align: center; align-items: center;">
@@ -86,10 +78,10 @@ nav {
 }
 
 .test{
-    background: none;
-    background-color: transparent;
-    background-image: none;
-  }
+  background: none;
+  background-color: transparent;
+  background-image: none;
+}
 
 nav a.router-link-exact-active {
   color: var(--color-text);
@@ -107,62 +99,6 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-.sidebar {
-  position: fixed;
-  left: -250px;
-  top: 0;
-  width: 250px;
-  height: 100%;
-  background-color: var(--color-background);
-  color: white;
-  transition: left 0.3s ease;
-  padding-top: 60px;
-  z-index: 1000;
-}
-
-.sidebar.open {
-  position: fixed;
-  left:0;
-  top:0;
-}
-
-.sidebar ul {
-  list-style: none;
-  padding: 0;
-}
-
-.sidebar ul li {
-  padding: 15px 20px;
-}
-
-.sidebar ul li a {
-  color: white;
-  text-decoration: none;
-}
-
-.content-shift {
-  margin-left: 200px;
-}
-
-.toggle-btn {
-  font-size: 24px;
-  color: white;
-  background: none;
-  border: none;
-  cursor: pointer;
-}
-
-.sidebar .toggle-btn {
-  position: absolute;
-  top: 10px;
-  right: -50px; /* Initially place the button outside the sidebar */
-}
-
-.sidebar.open .toggle-btn {
-  right: 10px; /* Move the button inside the sidebar when open */
-  transition: left 0.3s ease;
 }
 
 @media (min-width: 1024px) {
---
+++ dev/src/components/SidebarComponent.vue
@@ -0,0 +1,81 @@
+<template>
+  <div :class="['sidebar', { open: isOpen }]">
+    <button @click="toggleSidebar" class="toggle-btn">☰</button>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    toggleSidebar() {
+      this.isOpen = !this.isOpen;
+    },
+  }
+}
+</script>
+
+<style scoped>
+.sidebar {
+  position: fixed;
+  left: -250px;
+  top: 0;
+  width: 250px;
+  height: 100%;
+  background-color: var(--color-background);
+  color: white;
+  transition: left 0.3s ease;
+  padding-top: 60px;
+  z-index: 1000;
+}
+
+.sidebar.open {
+  left:0;
+}
+
+.sidebar ul {
+  list-style: none;
+  padding: 0;
+}
+
+.sidebar ul li {
+  padding: 15px 20px;
+}
+
+.sidebar ul li a {
+  color: white;
+  text-decoration: none;
+}
+
+.sidebar .toggle-btn {
+  font-size: 24px;
+  color: white;
+  background: none;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 10px;
+  right: -50px; /* Initially place the button outside the sidebar */
+}
+
+.sidebar.open .toggle-btn {
+  right: 10px; /* Move the button inside the sidebar when open */
+  transition: left 0.3s ease;
+}
+
+@media (min-width: 1024px) {
+}
+
+</style>
